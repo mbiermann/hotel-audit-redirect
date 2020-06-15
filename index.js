a3aa9ee9f56a7b1e347f42e6422f12b8f78ec833
Martin Biermann <martin@biermann.io>
2020-06-15 13:53:26 +0200

    Adding tracking

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const app = express();
 const authMiddleware = require('./utils/auth-middleware')
 const BadRequestError = require('./model/error/bad-request')
 const models = require('./model/url')
+const { measure } = require('measurement-protocol')
+const TRACK_EVENT_CATEGORY = 'End User Information Page'
 
 require('custom-env').env()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
+
+const trackEvent = (action, label) => {
+    measure(process.env.GA_TRACKING_ID).event(TRACK_EVENT_CATEGORY, action, label).send()
+}
 
 app.get('/favicon.ico', (req, res) => res.status(200))
 
@@ -20,13 +26,16 @@ app.get('/:url', async (req,res) => {
     try {
         let url = await models.findURL(req.params.url);
         if(url !== null) {
+            trackEvent('Redirect Open Success', url)
             res.redirect(url);
         } else {
+            trackEvent('Redirect Open Failure', req.params.url)
             res.send('invalid/expired URL');            
         }
     }
     catch(e) {
         console.log(e);
+        trackEvent('Redirect Open Error', req.params.url+":"+e.message)
         res.send('invalid/expired URL');
     }
 });
@@ -35,16 +44,20 @@ app.use(authMiddleware)
 
 app.post('/api/short', async (req,res) => {
     if(validUrl.isUri(req.body.url)) {
+        trackEvent('Redirect Create Success', req.body.url)
         // valid URL        
         try {
             let hash = await models.storeURL(req.body.url);
+            console.log(req.hostname + '/' +hash)
             res.send(req.hostname + '/' +hash);
         }
         catch(e) {
             console.log(e);
+            trackEvent('Redirect Create Error', req.body.url+":"+e.message)
             res.send('error occurred while storing URL.');
         }
     } else {
+        trackEvent('Redirect Create Failure', req.body.url)
         res.send('invalid URL');
     }
 });
